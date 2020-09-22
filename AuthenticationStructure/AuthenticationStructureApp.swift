a9050dd589a4290bebabd071ef5236d3e2b014a3
Michael SPilger <mispilger@gmail.com>
2020-09-22 13:02:24 +0200

    Initial Commit

---
+++ AuthenticationStructure/AuthenticationStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationStructureApp.swift
+//  AuthenticationStructure
+//
+//  Created by Spilger, Michael on 22.09.20.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationStructure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthenticationStructure
+//
+//  Created by Spilger, Michael on 22.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
